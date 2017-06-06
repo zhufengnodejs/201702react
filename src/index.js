8ca7b3d63d4e755b402d39b24f0081b25c086cb0
zhufengnodejs <zhufengnodejs@126.com>
2017-06-06 10:47:17 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+let id = 'id1';
+//React元素 -虚拟DOM元素
+//是以<DOM标签开头的
+//虚拟DOM元素属性不能随便写
+//要转换驼峰命名法的话 class class
+let style = {backgroundColor:'green'};
+let msg = `
+<script>while(true)alert('1');</script>
+`
+
+let ele = (
+    <h1 className="red" style={style}>
+        hello
+    </h1>
+)
+console.log(ele);
+// {type:'h1',props:{children:'hello',className:'red',style:{backgroundColor:'green'}}}
+ReactDOM.render(
+    ele,
+    document.querySelector('#root')
+);
