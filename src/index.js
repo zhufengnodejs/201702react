cc98e7e8d8535bc3ed4b259925dc551dc97b22a2
zhufengnodejs <zhufengnodejs@126.com>
2017-06-06 12:12:05 +0800

    复合组件

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import React,{Component} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 class Panel extends Component{
+  //this.props = {head=:"头",body:"体",footer:"尾"}
   render(){
       return (
-          <div>
-
+          <div className="panel panel-default">
+            <PanelHead content={this.props.head}/>
+            <PanelBody content={this.props.body}/>
+            <PanelFooter content={this.props.footer}/>
           </div>
       )
   }
 }
 class PanelHead extends Component{
-
+    render(){
+        return <div className="panel-heading">
+            {this.props.content}
+        </div>
+    }
 }
+
 class PanelBody extends Component{
-
+    render(){
+        return <div className="panel-body">{this.props.content}</div>
+    }
 }
-class PanelFooter extends Component{
 
+class PanelFooter extends Component{
+    render(){
+        return <div className="panel-footer">{this.props.content}</div>
+    }
 }
 render(<Panel head="头" body="体" footer=" 尾"/>,document.querySelector('#root'));
