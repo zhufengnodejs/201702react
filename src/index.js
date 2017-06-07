a6e0b3f91b1352f4d191c625520d0605b8274fb0
zhufengnodejs <zhufengnodejs@126.com>
2017-06-07 11:57:22 +0800

    复合组件

--- src/index.js
+++ src/index.js
@@ -3,30 +3,41 @@ import {render} from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 //分页组件
 class Pagination extends Component{
+    constructor(props){
+        super(props);
+        this.state = {current:this.props.pageNum};
+    }
+    changePage =(current)=>{
+        this.setState({current});
+    }
     render(){
         let lis = [];
+        if(this.state.current>1){
+            lis.push(<li key={0} onClick={()=>this.changePage(this.state.current-1)}>
+                <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>);
+        }
         for(let i=1;i<=this.props.totalPages;i++){
-            lis.push(<li key={i} className={i==this.props.pageNum?"active":""}><a href="#">{i}</a></li>);
+            lis.push(<li onClick={()=>this.changePage(i)} key={i} className={i==this.state.current?"active":""}><a href="#">{i}</a></li>);
+        }
+        if(this.state.current<this.props.totalPages){
+            lis.push(<li onClick={()=>this.changePage(this.state.current+1)} key={this.props.totalPages+1}>
+                <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>);
         }
         return (
             <nav>
                 <ul className="pagination">
-                    <li>
-                        <a href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
                     {
                         lis
                     }
-                    <li>
-                        <a href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
                 </ul>
             </nav>
         )
     }
 }
-render(<Pagination pageNum={2} totalPages={5}/>,document.querySelector('#root'));
+render(<Pagination pageNum={1} totalPages={5}/>,document.querySelector('#root'));
