26721c379c527ae463c09c1587f4a956f4b0ae54
zhufengnodejs <zhufengnodejs@126.com>
2017-06-08 17:49:18 +0800

    编写界面

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,6 +7,8 @@ export default class Slider extends Component{
     }
     turn = (n)=>{ // 切换
         let index = this.state.index + n;
+        if(index>=this.props.images.length)
+            index = 0;
         this.setState({index});
     }
     go = ()=>{
@@ -15,16 +17,22 @@ export default class Slider extends Component{
         },this.props.delay*1000)
     }
     componentDidMount(){
-        this.go();
+        if(this.props.autoplay)
+            this.go();
     }
     render(){
         let style={
-            width:200*this.props.images.length,
-            left:this.state.index*-200,
-            transitionDuration:'1s'
+            width:300*this.props.images.length,
+            left:this.state.index*-300,
+            transitionDuration:this.props.speed+'s'
+        }
+        let sliderOptions ={};
+        if(this.props.pause){
+            sliderOptions.onMouseOver =()=>clearInterval(this.$timer);
+            sliderOptions.onMouseOut = this.go;
         }
         return (
-            <div className="slider-wrapper">
+            <div className="slider-wrapper" {...sliderOptions}>
                 <ul className="sliders" style={style}>
                     {
                         this.props.images.map((image,index)=>(
